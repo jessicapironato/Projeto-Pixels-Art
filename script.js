64ca23549474b087fcd89f685b078093691daa52
Jéssica <jessicapmaximo@gmail.com>
2022-11-05 11:03:50 -0300

    requisito 6 e 7 ok

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // chamada dos elementos
 
 const botaoAleatorio = document.getElementById('button-random-color');
+const pixelQuadro = document.getElementById('pixel-board');
 
 // funções
 
+//para fazer cores aleatorias a cada click (somente o primeiro black)
 function corAleatoria() {
   const r = Math.ceil(Math.random() * 255);
   const g = Math.ceil(Math.random() * 255);
@@ -21,3 +23,9 @@ botaoAleatorio.addEventListener('click', () => {
     }
   }
 });
+
+// para fazer o quadro do pixel
+
+  for (let index = 0;  index < 25; index +=1) {
+    pixelQuadro.innerHTML += `<div class ="pixel" ></div>`
+  }
