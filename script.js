70c3e5feee90c67852d3668bd0585d76b8ad1e8d
Jéssica <jessicapmaximo@gmail.com>
2022-11-05 16:18:41 -0300

    refatoração global + requisito 5 ok

--- script.js
+++ script.js
@@ -1,31 +1,48 @@
-// chamada dos elementos
+// Event listeners
 
-const botaoAleatorio = document.getElementById('button-random-color');
-const pixelQuadro = document.getElementById('pixel-board');
-
-// funções
-
-//para fazer cores aleatorias a cada click (somente o primeiro black)
-function corAleatoria() {
-  const red = Math.ceil(Math.random() * 255);
-  const green = Math.ceil(Math.random() * 255);
-  const blue = Math.ceil(Math.random() * 255);
-  return `rgba(${red}, ${green}, ${blue})`;
+window.onload = () => {
+  criarPixelBoard();
+  if (localStorage.getItem('colorPalette') === null) {
+    salvarCores();
+  }
+  colorirQuadros();
 }
 
-botaoAleatorio.addEventListener('click', () => { 
-  const novaCor = document.getElementsByClassName('color');
-  for (let index = 0; index < novaCor.length; index += 1) {
-    if (index === 0) {
-      novaCor[index].style.backgroundColor = 'black';
-    } else {
-      novaCor[index].style.backgroundColor = corAleatoria();
-    }
-  }
+document.getElementById('button-random-color').addEventListener('click', () => {
+  salvarCores();
+  colorirQuadros();
 });
 
-// para fazer o quadro do pixel
+// Functions
 
+function criarPixelBoard() {
+  const pixelQuadro = document.getElementById('pixel-board');
   for (let index = 0;  index < 25; index +=1) {
     pixelQuadro.innerHTML += `<div class ="pixel" ></div>`
   }
+}
+
+function salvarCores() {
+  const cores = [];
+  cores.push('black');
+  cores.push(criarCorAleatoria());
+  cores.push(criarCorAleatoria());
+  cores.push(criarCorAleatoria());
+  localStorage.setItem('colorPalette', JSON.stringify(cores));
+}
+
+function criarCorAleatoria() {
+  const red = Math.ceil(Math.random() * 255);
+  const green = Math.ceil(Math.random() * 255);
+  const blue = Math.ceil(Math.random() * 255);
+
+  return `rgba(${red}, ${green}, ${blue})`;
+}
+
+function colorirQuadros() {
+  const quadros = document.getElementsByClassName('color');
+  const cores = JSON.parse(localStorage.getItem('colorPalette'));
+  for (let index = 0; index < quadros.length; index += 1) {
+    quadros[index].style.backgroundColor = cores[index];
+  }
+}
